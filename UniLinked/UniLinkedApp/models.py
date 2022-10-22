f63843c44720b22ebcce351d855b03dd7805c062
Robin Lee Simpson <robinleesimpson@Robins-MacBook-Pro.local>
2022-10-22 16:10:39 -0700

    Update

--- UniLinked/UniLinkedApp/models.py
+++ UniLinked/UniLinkedApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    user = models.CharField(max_length = 200)
+    email = models.EmailField(max_length = 200)
+    password = models.CharField(max_length = 200)
+    university = models.CharField(max_length=50)
+    major = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.user
+    
