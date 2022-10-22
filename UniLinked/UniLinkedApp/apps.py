ef4355779c435badaf69d30962d0239e90c217d7
Robin Lee Simpson <robinleesimpson@Robins-MacBook-Pro.local>
2022-10-22 09:37:52 -0700

    Fixed Django

--- UniLinked/UniLinkedApp/apps.py
+++ UniLinked/UniLinkedApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UnilinkedmainConfig(AppConfig):
+class UnilinkedappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'UniLinkedMain'
+    name = 'UniLinkedApp'
